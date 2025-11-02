09eb2cd1abac04427a2327e9c82a64d1e1274aeb
bhindu1216 <yadlabindhumadhuri2006@gmail.com>
2025-11-02 17:00:15 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://mhp-website-backend-svru.onrender.com";
     
     // Initialize token from localStorage
     const [token, setToken] = useState(() => localStorage.getItem("token") || "");
